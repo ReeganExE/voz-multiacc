4859498bc7a0249aa2dd1734edbdc5fee7a2d2d7
ReeganExE <dongian.rapclubkhtn@gmail.com>
2017-10-08 16:24:12 +0700

    Added build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const webpack = require('webpack');
 const path = require('path');
 const WriteFilePlugin = require('write-file-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const env = process.env;
 const DEV = env.NODE_ENV === 'development';
+const PROD = env.NODE_ENV === 'production';
 
 var options = {
   entry: {
@@ -38,6 +40,23 @@ var options = {
 
 if (DEV) {
   options.devtool = 'cheap-module-eval-source-map';
+} else if (PROD) {
+  options.plugins.push(new webpack.LoaderOptionsPlugin({
+    minimize: true,
+    debug: false
+  }));
+
+  options.plugins.push(
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        beautify: false,
+        ecma: 6,
+        compress: true,
+        comments: false
+      }
+    }));
+
+  options.devtool = 'nosources-source-map';
 }
 
 module.exports = options;
